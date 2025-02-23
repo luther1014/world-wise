ef11f12b003f383c589f6a6fc0b1d0dedd3f4f18
Luther <luther1096@gmail.com>
2025-02-24 00:07:36 +0530

    react router configuration with all the pages + nested routes + fetchig and displaying data from fake api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,58 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import "./index.css";
+import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
+
 function App() {
-  return <div>WorldWise</div>;
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (e) {
+        alert("There was an error", e);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="product" element={<Product />}></Route>
+        <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          ></Route>
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          ></Route>
+          <Route path="form" element={<p>Form page</p>}></Route>
+        </Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,28 @@
+import Message from "./Message";
+import Spinner from "./Spinner";
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+
+function CountryList({ cities, isLoading }) {
+  const countries = cities.reduce((acc, city) => {
+    if (!acc.map((c) => c.country).includes(city.country)) {
+      return [...acc, { country: city.country, emoji: city.emoji }];
+    } else {
+      return acc;
+    }
+  }, []);
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return <Message message="Add your first city by clicking on the map" />;
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
